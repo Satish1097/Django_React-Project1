31d177827f6a11883cd22177a0e152f5c20ac09b
Avinash <2019avinash2001@gmail.com>
2024-07-01 15:22:05 +0530

    Images folder created and Home jsx

--- EcoVegReact/src/App.jsx
+++ EcoVegReact/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Home } from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <Home />
     </>
   )
 }
