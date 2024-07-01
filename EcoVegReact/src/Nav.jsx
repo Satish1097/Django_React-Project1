eea158e7bcf83304fcf5d4b8f3f5e5332a86409a
satish <2019kumarsatish2019@gmail.com>
2024-07-01 15:09:40 +0530

    nav

--- EcoVegReact/src/Nav.jsx
+++ EcoVegReact/src/Nav.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './Nav.css'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <>
+    <div className="nav">
+      <div className="logo">
+        <img src="Images/Ecologo.png" alt="img" />
+        <h2>EcoVeg</h2>
+      </div>
+      <div className="search">
+        <div className="category">
+          <select name="" id="">
+            <option value="">All Category</option>
+            <option value="">Leaves</option>
+            <option value="">Root</option>
+            <option value="">Flowers</option>
+          </select>
+        </div>
+        <div className="input">
+          <form action="">
+            <input type="text" placeholder='Search here'/>
+          </form>
+        </div>
+        <div className="sicon">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+
+      </div>
+    </div>
+    </>
   )
 }
 
