76f924b39275b25ee0b34cf71b843172349ab4c3
satish <2019kumarsatish2019@gmail.com>
2024-07-02 11:39:21 +0530

    responsive Nav

--- EcoVegReact/src/Nav.jsx
+++ EcoVegReact/src/Nav.jsx
@@ -3,45 +3,58 @@ import { Link } from 'react-router-dom'
 import './Nav.css'
 
 function Nav() {
+
+  const OpenMenu = () => {
+    var x = document.getElementById('menu');
+    var y = document.getElementById('icon');
+    if (x.style.left == '-100%') {
+      x.style.left = '0px';
+      y.className = 'fa fa-times';
+    }else{
+      x.style.left='-100%';
+      y.className='fa-solid fa-bars-staggered';
+    }
+  }
   return (
     <>
-    <div className="nav">
-      <div className="logo">
-        <img src="Images/Ecologo.png" alt="img" />
-        <h2>EcoVeg</h2>
-      </div>
-      <div className="search">
-        <div className="category">
-          <select name="" id="">
-            <option value="">All Category</option>
-            <option value="">Leaves</option>
-            <option value="">Root</option>
-            <option value="">Flowers</option>
-          </select>
+      <div className="nav">
+        <div className="logo">
+          <img src="Images/Ecologo.png" alt="img" />
+          <h2>EcoVeg</h2>
         </div>
-        <div className="input">
-          <form action="">
-            <input type="text" placeholder='Search here'/>
-          </form>
+        <button className='bar' onClick={OpenMenu}><i className="fa-solid fa-bars-staggered" id='icon'></i></button>
+        <div className="search">
+          <div className="category">
+            <select name="" id="">
+              <option value="">All Category</option>
+              <option value="">Leaves</option>
+              <option value="">Root</option>
+              <option value="">Flowers</option>
+            </select>
+          </div>
+          <div className="input">
+            <form action="">
+              <input type="text" placeholder='Search here' />
+            </form>
+          </div>
+          <div className="sicon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
         </div>
-        <div className="sicon">
-        <i class="fa-solid fa-magnifying-glass"></i>
+        <div className="menu" id='menu'>
+          <ul>
+            <Link>Home</Link>
+            <Link>About</Link>
+            <Link>Services</Link>
+          </ul>
+        </div>
+        <div className="Login">
+          <Link><span><i class="fa-solid fa-user"></i></span></Link>
+        </div>
+        <div className="cart">
+          <Link><i class="fa-solid fa-cart-shopping"></i></Link>
         </div>
       </div>
-      <div className="menu">
-        <ul>
-          <Link>Home</Link>
-          <Link>About</Link>
-          <Link>Services</Link>
-        </ul>
-      </div>
-      <div className="Login">
-        <Link><span><i class="fa-solid fa-user"></i></span>Login</Link>
-      </div>
-      <div className="cart">
-        <Link><i class="fa-solid fa-cart-shopping"></i>Cart</Link>
-      </div>
-    </div>
     </>
   )
 }
