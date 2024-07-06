d3c22d1607d424a27fad70900146242eb6f0411f
Avinash <2019avinash2001@gmail.com>
2024-07-06 14:18:59 +0530

    Offer section edited and create responsive design

--- EcoVegReact/src/Offer.jsx
+++ EcoVegReact/src/Offer.jsx
@@ -7,15 +7,43 @@ const Offer = () => {
                 <div className="inner-container">
                     <div className="row-left">
                         <div className="row-left-inner">
-                            abc
+                            <div className="offer-data">
+                                <div className="offer-data-inner">
+                                    <div className='offer-section-content'>
+                                        <h1>Items on SALE</h1>
+                                        <p className='offer-p'>Get up to 50% off</p>
+                                        <a href="#" className="offer-section-btn">Shop Now</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="row-right">
                         <div className="row-right-top">
-                            <div className="inner">xyz</div>
+                            <div className="inner">
+                                <div className="offer-data">
+                                    <div className="offer-data-inner">
+                                        <div className='offer-section-content'>
+                                            <h1>Combo Offers</h1>
+                                            <p className='offer-p'>Discount up to 40% </p>
+                                            <a href="#" className="offer-section-btn">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="row-right-below">
-                            <div className="inner">abc</div>
+                            <div className="inner">
+                                <div className="offer-data">
+                                    <div className="offer-data-inner">
+                                        <div className='offer-section-content'>
+                                            <h1>Discount Coupons</h1>
+                                            <p className='offer-p'>Discount up to 30%</p>
+                                            <a href="#" className="offer-section-btn">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
