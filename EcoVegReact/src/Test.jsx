caa4b3faae5af51924cb111a2789fa24316c0140
Avinash <2019avinash2001@gmail.com>
2024-07-10 13:46:03 +0530

    project

--- EcoVegReact/src/Test.jsx
+++ EcoVegReact/src/Test.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 
-function Test() {
-  const [customers, setCustomers] = useState([]);
+// function Test() {
+//   const [customers, setCustomers] = useState([]);
 
-  useEffect(() => {
-    // Fetch Customers
-    axios.get('http://127.0.0.1:8000/customers/')  // Update the URL if necessary
-      .then(response => setCustomers(response.data))
-      .catch(error => console.error('Error fetching customers:', error));
-  },[]);
+//   useEffect(() => {
+//     // Fetch Customers
+//     axios.get('http://127.0.0.1:8000/customers/')  // Update the URL if necessary
+//       .then(response => setCustomers(response.data))
+//       .catch(error => console.error('Error fetching customers:', error));
+//   },[]);
 
-  return (
-    <div className="App">
-      <h1>Customers</h1>
-      <ul>
-        {customers.map(customer => (
-          <li key={customer.id}>{customer.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <h1>Customers</h1>
+//       <ul>
+//         {customers.map(customer => (
+//           <li key={customer.id}>{customer.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Test;
+// export default Test;
